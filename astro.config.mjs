852e0c14c53eb175bc34c06550e3799c74dc3004
JoshuaGarnaat <134138@stanislascollege.net>
2026-02-27 23:06:04 +0100

    Add sitemap-index.xml and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
 	markdown: {
@@ -15,5 +16,9 @@ export default defineConfig({
 				}
 			]
 		]
-	}
+	},
+	site: 'https://driedubbel.com',
+	integrations: [
+		sitemap()
+  	],
 })
